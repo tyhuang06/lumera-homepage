b538bc726c68fd6fc5446d74929194418e2eb0f9
Jamie Huang <tyhuang06@gmail.com>
2026-02-01 00:21:40 -0800

    create website footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import './App.css';
+import { Footer } from './components/Footer';
 import { Navbar } from './components/Navbar';
 
 function App() {
 	return (
 		<>
 			<Navbar />
-			<div className="pt-14">
-				<div>Body</div>
+			<div className="pt-14 pl-4">
+				<div>Images</div>
 			</div>
+			<Footer />
 		</>
 	);
 }
